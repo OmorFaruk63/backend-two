24558b808b70b09bba2504250d67125f955a7001
OmorFaruk63 <omor3710@gmail.com>
2024-10-26 12:03:26 +0600

    add country name

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const famousCountries = [
+  { name: "United States" },
+  { name: "Canada" },
+  { name: "United Kingdom" },
+  { name: "Germany" },
+  { name: "France" },
+  { name: "Italy" },
+  { name: "Spain" },
+  { name: "Australia" },
+  { name: "Japan" },
+  { name: "China" },
+  { name: "India" },
+  { name: "Brazil" },
+  { name: "Mexico" },
+  { name: "Russia" },
+  { name: "South Korea" },
+  { name: "Saudi Arabia" },
+  { name: "Netherlands" },
+  { name: "Switzerland" },
+  { name: "South Africa" },
+  { name: "Turkey" },
+];
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-app.get("/home", (req, res) => {
-  res.send("<h1>Hello World! From Home</h1>");
+  res.send(famousCountries);
 });
 
 app.listen(process.env.PORT, () => {
